340a81b78ea4c2442786a95acd2c30926348375a
Jayant Ghadge <59535571+Dextro99-flak@users.noreply.github.com>
2026-07-27 15:52:08 +0530

    Fix indentation for make_transforms function

--- src/transforms.py
+++ src/transforms.py
@@ -1,7 +1,6 @@
 import torchvision.transforms as T
 def make_transforms(*args, **kwargs):
-# Only spatial transforms are safe for 224-channel arrays
-return T.Compose([
-    T.RandomHorizontalFlip(),
-    T.RandomVerticalFlip()
-])
+    return T.Compose([
+        T.RandomHorizontalFlip(),
+        T.RandomVerticalFlip()
+    ])
