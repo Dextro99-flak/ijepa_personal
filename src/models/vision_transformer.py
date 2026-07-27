10cfc55b84eb56b9b9305d3b7dd3391efed2c4d5
Jayant Ghadge <59535571+Dextro99-flak@users.noreply.github.com>
2026-07-27 15:14:12 +0530

    Modify VisionTransformer parameters and patch sizes
    Updated the VisionTransformer parameters for image size, patch size, and input channels. Adjusted patch sizes for various model configurations.

--- src/models/vision_transformer.py
+++ src/models/vision_transformer.py
@@ -333,9 +333,9 @@ class VisionTransformer(nn.Module):
     """ Vision Transformer """
     def __init__(
         self,
-        img_size=[64],
-        patch_size=2,
-        in_chans=188,
+        img_size=[128],
+        patch_size=8,
+        in_chans=224,
         embed_dim=768,
         predictor_embed_dim=384,
         depth=12,
@@ -451,42 +451,42 @@ def vit_predictor(**kwargs):
     return model
 
 
-def vit_tiny(patch_size=2, **kwargs):
+def vit_tiny(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
 
-def vit_small(patch_size=2, **kwargs):
+def vit_small(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
 
-def vit_base(patch_size=2, **kwargs):
+def vit_base(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
 
-def vit_large(patch_size=2, **kwargs):
+def vit_large(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
 
-def vit_huge(patch_size=2, **kwargs):
+def vit_huge(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=1280, depth=32, num_heads=16, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
 
-def vit_giant(patch_size=2, **kwargs):
+def vit_giant(patch_size=8, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=1408, depth=40, num_heads=16, mlp_ratio=48/11,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
